c5fa4117a6d868e1e3678b7befd45140ef259c18
CXPhoenix <jate9229@gmail.com>
2025-06-22 23:57:59 +0800

    :sparkles: Update project name and add configuration enhancements
    This commit renames the project from "FHSH AiSP Task Board" to "FHSH AiSP Mission Board" in the README. It also enhances the configuration setup by introducing new environment configuration classes for system and app settings, updates MongoDB connection parameters, and adds session middleware to FastAPI. Additionally, it includes example environment files, updates the .gitignore to secure sensitive data, and adds untracked files like Docker configurations for future setup.

--- app/configs/__init__.py
+++ app/configs/__init__.py
@@ -1,20 +1,46 @@
+import typing
+
+from pydantic import field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+from uvicorn.config import logger
 
 class EnvConf(BaseSettings):
     model_config = SettingsConfigDict(case_sensitive=False, extra="ignore")
 
+
 class DbConf(EnvConf):
-    model_config = SettingsConfigDict(env_prefix='MONGODB_')
-    
+    model_config = SettingsConfigDict(env_prefix="MONGODB_")
+
     host: str = "mongo"
     port: int = 27017
-    username: str = "username"
-    password: str = "password"
+    initdb_root_username: str = "<your-username>"
+    initdb_root_password: str = "<your-password>"
     auth_source: str = "admin"
 
+
 class SystemConf(EnvConf):
-    # TODO: Set Configurations
-    ...
+    model_config = SettingsConfigDict(env_prefix="SYSTEM_")
+    
+    session_secret: str = "<your-session-secret>"
+
+
+class AppConf(EnvConf):
+    model_config = SettingsConfigDict(env_prefix="APP_")
+    
+    userdb: str = "<your-userdb>"
+    admins: str | list[str] = []
+    
+    @field_validator('admins', mode='after')
+    @classmethod
+    def seperate_admin_list(cls, v: str) -> list[str]:
+            if isinstance(v, str):
+                return v.split(':')
+            return v
+
 
 db_conf = DbConf()
+sys_conf = SystemConf()
+app_conf = AppConf()
+
+__all__ = [db_conf, sys_conf, app_conf]
