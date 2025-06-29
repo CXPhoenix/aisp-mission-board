769564d67f79d855b6e3dfbc866ca8553956d674
CXPhoenix <jate9229@gmail.com>
2025-06-29 15:51:52 +0800

    :art: Refactor configuration classes for improved clarity and consistency

--- app/configs/__init__.py
+++ app/configs/__init__.py
@@ -3,7 +3,6 @@ import typing
 from pydantic import field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-from uvicorn.config import logger
 
 class EnvConf(BaseSettings):
     model_config = SettingsConfigDict(case_sensitive=False, extra="ignore")
@@ -14,29 +13,32 @@ class DbConf(EnvConf):
 
     host: str = "mongo"
     port: int = 27017
-    initdb_root_username: str = "<your-username>"
-    initdb_root_password: str = "<your-password>"
+    username: str = "<your-username>"
+    password: str = "<your-password>"
     auth_source: str = "admin"
 
 
 class SystemConf(EnvConf):
     model_config = SettingsConfigDict(env_prefix="SYSTEM_")
-    
+
     session_secret: str = "<your-session-secret>"
 
 
 class AppConf(EnvConf):
     model_config = SettingsConfigDict(env_prefix="APP_")
-    
+
+    title: str = "<your-app-title>"
     userdb: str = "<your-userdb>"
+    malldb: str = "<your-malldb>"
+    recorddb: str = "<your-recorddb>"
     admins: str | list[str] = []
-    
-    @field_validator('admins', mode='after')
+
+    @field_validator("admins", mode="after")
     @classmethod
     def seperate_admin_list(cls, v: str) -> list[str]:
-            if isinstance(v, str):
-                return v.split(':')
-            return v
+        if isinstance(v, str):
+            return v.split(":")
+        return v
 
 
 db_conf = DbConf()
